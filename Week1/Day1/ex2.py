82716db3b77868afcb65511da8e4a7911f5e4af0
Arielle5785 <arielle.benadi@gmail.com>
2024-11-03 15:08:14 +0200

    exercise xp 1 mndatory

---
+++ Week1/Day1/ex2.py
@@ -0,0 +1,25 @@
+#first_name = input ('what is your First Name?')
+#last_name = input ('what is your Last Name?')
+#print (f'Hello {first_name} {last_name}, have a nice day ahead.')
+
+#print('python' is 'python')
+#print('python' is not 'python')
+
+#print('HTML' is not 'CSS' and 'Python' is 'JavaScript')
+
+#if 'HTML' is not 'CSS' and 'Python' is not 'JavaScript':
+#    print('That\'s right!')
+
+
+# my_age = 34
+# my_age +=1
+# print(my_age)
+
+age = int(input ("How old are you?"))
+#print(f'You are {age} years old')
+if age >18  and age < 35:
+    print('You can enter the club')
+elif age < 18 and age >12:
+    print('You are a teenager')
+else:
+    print ('You are an adult')
