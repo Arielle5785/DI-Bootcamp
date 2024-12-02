6466352a096a689c163c81bb3fd0f568c660d07c
Arielle5785 <arielle.benadi@gmail.com>
2024-12-02 17:37:07 +0200

    Daily Challenge 1 Delivered

---
+++ Week5/Day2/DailyChallenge/DCScript1.js
@@ -0,0 +1,45 @@
+const gameInfo = [
+ {
+   username: "john",
+   team: "red",
+   score: 5,
+   items: ["ball", "book", "pen"]
+ },
+ {
+   username: "becky",
+   team: "blue",
+   score: 10,
+   items: ["tape", "backpack", "pen"]
+ },
+ {
+   username: "susy",
+   team: "red",
+   score: 55,
+   items: ["ball", "eraser", "pen"]
+ },
+ {
+   username: "tyson",
+   team: "green",
+   score: 1,
+   items: ["book", "pen"]
+ },
+];
+// const usernames = gameInfo.forEach(player => {console.log(
+//     `${player.username}!`)
+// });
+
+// Create an array using forEach that contains the usernames of all players with a score bigger than 5.
+// The new array should look like this :
+
+// const winners = ["becky", "susy"]
+
+// const usernames = gameInfo.filter(player => player.score >5).forEach(player => {console.log(
+//     `${player.username}!`)
+// });
+
+// Find and display the total score of the users. (Hint: The total score is 71)
+
+const totalScore = gameInfo.reduce((acc, player) => acc + player.score,0);
+console.log(totalScore);
+
+
