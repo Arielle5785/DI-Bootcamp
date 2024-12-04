51972869c2c78637af9d412a04f0ac6d28d9a1b8
Arielle5785 <arielle.benadi@gmail.com>
2024-12-04 14:30:02 +0200

    Daily Challenge delivered

---
+++ Week5/Day4/DailyChallenge/DCscript.js
@@ -0,0 +1,12 @@
+document.querySelector('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const lastname = document.getElementById('lastname').value;
+    const userName = { name, lastname };
+    const jsonUserName = JSON.stringify(userName);
+    console.log(jsonUserName);
+    const jsonDisplay = document.createElement('p');
+    document.body.appendChild(jsonDisplay);
+    // document.getElementById('name').value = '';
+    // document.getElementById('lastname').value = '';
+});
