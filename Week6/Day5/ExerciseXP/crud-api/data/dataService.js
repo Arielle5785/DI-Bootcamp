dcbddb76433c94b8df953e061fd26c486195cd6d
Arielle5785 <arielle.benadi@gmail.com>
2024-12-22 19:35:44 +0200

    crud-api with axios

--- Week6/Day5/ExerciseXP/crud-api/data/dataService.js
+++ Week6/Day5/ExerciseXP/crud-api/data/dataService.js
@@ -1,19 +1,31 @@
 const axios = require('axios')
 
 
-const fetchPosts = async () => { 
-    try {
-        const res = await fetch('https://jsonplaceholder.typicode.com/posts'); 
-        if (!res.ok) { 
-            throw new Error(`HTTP error! Status: ${res.status}`);
-        }
-        const data = await res.json();
-        console.log(data); 
-    } catch (error) {
-        console.error('Error fetching posts:', error); 
-    }
-};
+// const fetchPosts = async () => {
+//     try {
+//         const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//         if (!res.ok) {
+//             throw new Error(`HTTP error! Status: ${res.status}`);
+//         }
+//         const data = await res.json();
+//         console.log(data);
+//     } catch (error) {
+//         console.error('Error fetching posts:', error);
+//     }
+// };
 
-module.exports = {
-    fetchPosts,
-}
+// module.exports = {
+//     fetchPosts,
+// }
+
+
+async function fetchPosts(url) {
+    try {
+      const response = await axios.get(url);
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+module.exports = {fetchPosts}
