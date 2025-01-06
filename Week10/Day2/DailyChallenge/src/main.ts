456a1e599d8e5cb5c19a54c12c54ee5d5499d3c6
Arielle5785 <arielle.benadi@gmail.com>
2025-01-06 16:51:28 +0200

    delivers Daily

---
+++ Week10/Day2/DailyChallenge/src/main.ts
@@ -0,0 +1,19 @@
+const validateUnionType =(value: any, allowedTypes: string[]): boolean =>{
+    const valueType = Array.isArray(value) ? "array" : typeof value;
+    return allowedTypes.includes(valueType);
+}
+    
+//Example of usage:
+const value1 = "Hello the world";
+const value2 = 42;
+const value3 = true;
+const value4 = ["string1", "string2", "string3"]
+// const allowedTypes = ["string"]//true, false,false,false
+// const allowedTypes = ["string", "array"]//true, false,false, true
+// const allowedTypes = ["string", "number", "array"]//true, true, false, true
+const allowedTypes=["string", "number", "array", "boolean"]//true, true, true, true
+
+console.log(validateUnionType(value1, allowedTypes));
+console.log(validateUnionType(value2, allowedTypes));
+console.log(validateUnionType(value3, allowedTypes));
+console.log(validateUnionType(value4, allowedTypes));
