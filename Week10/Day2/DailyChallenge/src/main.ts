607aaa6392db4fec8837493d7fb44db0fad8a82e
Arielle5785 <arielle.benadi@gmail.com>
2025-01-06 16:52:19 +0200

    delivers the validateUnionType exrecise

--- Week10/Day2/DailyChallenge/src/main.ts
+++ Week10/Day2/DailyChallenge/src/main.ts
@@ -8,10 +8,10 @@ const value1 = "Hello the world";
 const value2 = 42;
 const value3 = true;
 const value4 = ["string1", "string2", "string3"]
-// const allowedTypes = ["string"]//true, false,false,false
+const allowedTypes = ["string"]//true, false,false,false
 // const allowedTypes = ["string", "array"]//true, false,false, true
 // const allowedTypes = ["string", "number", "array"]//true, true, false, true
-const allowedTypes=["string", "number", "array", "boolean"]//true, true, true, true
+// const allowedTypes=["string", "number", "array", "boolean"]//true, true, true, true
 
 console.log(validateUnionType(value1, allowedTypes));
 console.log(validateUnionType(value2, allowedTypes));
